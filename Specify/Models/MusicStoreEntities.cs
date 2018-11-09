9d7850144db735d0af3ae77297b461b0d0ee5545
dvcsgit <ksitri@outlook.com>
2018-11-09 16:47:40 +0800

    Edit forms using scaffolding.

---
+++ Specify/Models/MusicStoreEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Specify.Models
+{
+    public class MusicStoreEntities:DbContext
+    {
+        public MusicStoreEntities():base("name=MusicStoreEntities")//Dbname or connectionString name
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
